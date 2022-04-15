f5c3dc5ab5a7d7043fddcb27eea219c8e9d5c2ba
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-04-15 17:04:48 +0300

    Added the routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
+// import { Component } from '@angular/core'; 
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AboutComponent } from './about/about.component';
+import { GitspotComponent } from './gitspot/gitspot.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+const routes: Routes = [
+  {path: 'gitspot', component: GitspotComponent},
+  {path: 'about', component: AboutComponent},
+
+  { path: '', redirectTo:"/gitspot", pathMatch:"full"},//redirects to goals when user enters an empty path
+  { path:'**', component: NotFoundComponent},//for error 404
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
