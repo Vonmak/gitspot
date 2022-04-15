582d67c1e146b107153a5df04d52f871bef2e8c7
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-04-15 17:39:45 +0300

    added a search form.

---
+++ src/app/git-form/git-form.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-git-form',
+  templateUrl: './git-form.component.html',
+  styleUrls: ['./git-form.component.css']
+})
+export class GitFormComponent implements OnInit {
+
+  // searchName:string;
+  @Output() searchOutput = new EventEmitter<any>()
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  search(){
+    // this.searchOutput.emit(this.searchName);
+    // this.searchName = "";
+  }
+
+}
