d4dbdb2b1002da6c36ee5ab8b7136bc700c9f747
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-04-15 11:48:37 +0300

    added a new parent component gitspot

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GitspotComponent } from './gitspot/gitspot.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GitspotComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/gitspot/gitspot.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gitspot',
+  templateUrl: './gitspot.component.html',
+  styleUrls: ['./gitspot.component.css']
+})
+export class GitspotComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
