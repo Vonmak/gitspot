a1bf7d10f0589182063a59e951cc69c20692973b
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-04-18 18:28:08 +0300

    updated the AppModule since i deleted the gitspot component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GitspotComponent } from './gitspot/gitspot.component';
+// import { GitspotComponent } from './gitspot/gitspot.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AboutComponent } from './about/about.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { GitFormComponent } from './git-form/git-form.component';
+import { RepositoryComponent } from './repository/repository.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GitspotComponent,
+    
     NavbarComponent,
     AboutComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    GitFormComponent,
+    RepositoryComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     HttpClientModule
   ],
   providers: [],
