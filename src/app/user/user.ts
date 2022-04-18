fe420e17cf81c07e56fcd3dfebf8959c46802f9f
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-04-18 18:22:16 +0300

    updated the user class with new variables and parameters.

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,5 +1,5 @@
 export class User {
     constructor(
-       public name: string, public avatar-url:string, public username:string, public repos:number
+       public name: string, public html_url:string, public avatar_url:string, public login:string, public public_repos:number, public followers:number, public following:number,
     ){}
 }
