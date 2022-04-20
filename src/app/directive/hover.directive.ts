381990c55f0759560f2c9fb1933f1a1c3f9e55a5
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-04-20 06:14:36 +0300

    added a directive hover  and the files needed for it to work in my project.

---
+++ src/app/directive/hover.directive.ts
@@ -0,0 +1,30 @@
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appHover]'
+})
+export class HoverDirective {
+
+  constructor(private elem:ElementRef) { }
+
+  private backgroundColor(action:string){
+    this.elem.nativeElement.style.backgroundColor = action;
+  }
+  private color(action:string){
+    this.elem.nativeElement.style.color = action;
+    
+  };
+  
+
+  @HostListener('mouseenter')onMouseEnter(){
+    this.color('#F0F8FF');
+    this.backgroundColor('#405061')
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.color('');
+    this.backgroundColor('')
+  }
+
+
+}
