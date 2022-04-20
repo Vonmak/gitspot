53bf1d28db478916ec70735b738e59e8a1c16218
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-04-20 10:47:37 +0300

    updated the approuting module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,14 @@
-// import { Component } from '@angular/core'; 
+import { Component } from '@angular/core'; 
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AboutComponent } from './about/about.component';
-// import { GitspotComponent } from './gitspot/gitspot.component';
 import { GitFormComponent } from './git-form/git-form.component';
-import { NotFoundComponent } from './not-found/not-found.component';
+// import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
  
-  { path: '', redirectTo:"/git-form", pathMatch:"full"},//redirects to goals when user enters an empty path
+  { path: '', redirectTo:"/git-form", pathMatch:"full"},//redirects to gitForm when user enters an empty path
   {path:'git-form', component:GitFormComponent},
   {path: 'about', component: AboutComponent},
   { path:'**', component: GitFormComponent},
