6db4a2c2a5ff4f2ce8275261d5d91379776034c5
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-04-20 09:23:27 +0300

    moved my functions from the main component to services.

--- src/app/services/repo/repo.service.ts
+++ src/app/services/repo/repo.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { UserService } from '../user/user.service';
+// import { User } from 'src/app/user/user';
+// import { UserService } from '../user/user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RepoService {
 
-  constructor() { }
+  username:string;
+
+  constructor(private http:HttpClient) {
+    this.username = 'Vonmak';
+   }
+  getProfileRepos(){
+    return this.http.get('https://api.github.com/users/' + this.username + '/repos')
+  }
+
+  updateProfile(username: string) {
+    this.username = username;
+  }
 }
--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from 'src/app/user/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  user!:User;
+  private username:string;
+
+  constructor(private http:HttpClient) {
+    this.username = 'Vonmak';
+   }
+
+  getProfileInfo(){
+    interface ApiResponse{
+      login:string;
+    }
+    return this.http.get('https://api.github.com/users/' + this.username)
+  }
+
+  updateProfile(username: string) {
+    this.username = username;
+  }
 }
