eda2994352a316f70efa7128b376d95d2802ec42
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-04-20 06:12:19 +0300

    updated my project header name.

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-  title = 'GitSpot';
+  title = 'Git Account Spot';
   constructor() { }
 
   ngOnInit(): void {
