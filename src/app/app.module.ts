b02881133a51053192ff5cd161bf9bc57e17fd9b
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-04-20 06:16:40 +0300

    the pipe and the directive automaticallly initilized in appmodule.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-// import { GitspotComponent } from './gitspot/gitspot.component';
+
 import { NavbarComponent } from './navbar/navbar.component';
 import { AboutComponent } from './about/about.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { GitFormComponent } from './git-form/git-form.component';
 import { RepositoryComponent } from './repository/repository.component';
+import { DaysPipe } from './pipe/days.pipe';
+import { HoverDirective } from './directive/hover.directive';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,9 @@ import { RepositoryComponent } from './repository/repository.component';
     AboutComponent,
     NotFoundComponent,
     GitFormComponent,
-    RepositoryComponent
+    RepositoryComponent,
+    DaysPipe,
+    HoverDirective
   ],
   imports: [
     BrowserModule,
